87e4dafebad93c9360ba51a8e1018bd74cb1e077
syborg64 <julian.scott@epitech.eu>
2025-03-12 19:54:05 +0900

    fix(ShaderBuilder): match case of pbMaterial

--- src/engine/rendering/pipeline.cpp
+++ src/engine/rendering/pipeline.cpp
@@ -19,11 +19,11 @@ void ShaderBuilder<pipeline>::build(ShaderProgram &shader) {
   shader.addUniform(pipeline::uniforms::view::name);
   shader.addUniform(pipeline::uniforms::invView::name);
 
-  shader.addUniform(pipeline::uniforms::PbrMaterial::custom_ambient::name);
-  shader.addUniform(pipeline::uniforms::PbrMaterial::diffuse::name);
-  shader.addUniform(pipeline::uniforms::PbrMaterial::specular::name);
-  shader.addUniform(pipeline::uniforms::PbrMaterial::roughness::name);
-  shader.addUniform(pipeline::uniforms::PbrMaterial::emit::name);
-  shader.addUniform(pipeline::uniforms::PbrMaterial::halflambert::name);
-  shader.addUniform(pipeline::uniforms::PbrMaterial::normal_mode::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::custom_ambient::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::diffuse::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::specular::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::roughness::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::emit::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::halflambert::name);
+  shader.addUniform(pipeline::uniforms::pbrMaterial::normal_mode::name);
 }
