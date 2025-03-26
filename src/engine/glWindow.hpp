5eb27a601a0cf439516101f76da367699e2f4f20
syborg64 <julian.scott@epitech.eu>
2025-03-26 14:50:08 +0900

    feat(glWindow): x11 hints and debug log

--- src/engine/glWindow.hpp
+++ src/engine/glWindow.hpp
@@ -220,6 +220,9 @@ class glWindow : public cevy::engine::Window::generic_window {
   }
 
   bool init_context() {
+#if GLFW_HINT_X11
+    glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif // GLFW_HINT_X11
     if (!glfwInit()) {
       throw std::runtime_error("failed to init glfw");
       // Initialization failed
@@ -278,7 +281,17 @@ class glWindow : public cevy::engine::Window::generic_window {
       // fprintf(stderr, "OpenGL 4.2 not supported\n");
       return -1;
     }
-#endif // _WIN32
+#endif // _WIN32 | __linux__
+
+    glEnable(GL_DEBUG_OUTPUT);
+    // typedef void APIENTRY _DEBUGPROC(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
+
+    auto debug_func = [](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) -> void {
+      std::cerr << "DBG::" << source << "::{" << type << "}" << std::endl << std::string(message) << std::endl << std::endl;
+      // std::cerr << "DBG::" << "::{" << type << "}" << std::string(message) << std::endl;
+    };
+
+    glDebugMessageCallback(debug_func, nullptr);
 
     printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION),
            glGetString(GL_SHADING_LANGUAGE_VERSION));
