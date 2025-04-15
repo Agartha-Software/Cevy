e1731933aa1f06a729ca7f356b0f7dda311adfea
syborg64 <julian.scott@epitech.eu>
2025-04-15 20:24:20 +0900

    fix(Time): use this-> accessor

--- src/ecs/Time.hpp
+++ src/ecs/Time.hpp
@@ -19,26 +19,24 @@ class Time {
   std::chrono::duration<double, std::ratio<1>> _last_update_delta;
   double currentTimescale = 1;
   double nextTimescale = 1;
-  size_t frameCount = 0;;
+  size_t frameCount = 0;
 
   public:
   std::chrono::duration<double, std::ratio<1>> uptime() const {
-    return std::chrono::high_resolution_clock::now() - _first_update;
+    return this->_last_update - this->_first_update;
   }
 
   void update_with_instant(std::chrono::time_point<std::chrono::high_resolution_clock> &&instant);
 
-  std::chrono::duration<double, std::ratio<1>> raw() const { return _last_update_delta; }
+  std::chrono::duration<double, std::ratio<1>> raw() const { return this->_last_update_delta; }
 
-  double delta_seconds() const { return _last_update_delta.count() * timescale(); }
+  double delta_seconds() const { return this->_last_update_delta.count() * this->timescale(); }
 
-  double timescale() const {
-    return currentTimescale;
-  }
+  double timescale() const { return this->currentTimescale; }
 
   double setTimescale(double next) {
-    auto old = nextTimescale;
-    nextTimescale = next;
+    auto old = this->nextTimescale;
+    this->nextTimescale = next;
     return old;
   }
   size_t frame() const { return this->frameCount; }
@@ -49,6 +47,5 @@ class Time {
   static void init_timer(cevy::ecs::World &w);
   static void start_timer(cevy::ecs::Resource<cevy::ecs::Time> time);
   static void update_timer(cevy::ecs::Resource<cevy::ecs::Time> time);
-
 };
 } // namespace cevy::ecs
