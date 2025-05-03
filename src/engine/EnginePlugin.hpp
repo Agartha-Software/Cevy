7a0341465d3fae49ee7d187cd5be035aaf15c302
syborg64 <julian.scott@epitech.eu>
2025-05-03 23:28:19 +0900

    feat(EnginePlugin): move children system to PostUpdate

--- src/engine/EnginePlugin.hpp
+++ src/engine/EnginePlugin.hpp
@@ -63,7 +63,7 @@ class Engine : public cevy::ecs::Plugin {
     app.add_plugins(cevy::input::InputPlugin());
     app.add_plugins(typename Windower::Plugin());
     app.add_systems<cevy::engine::PreRenderStage>(update_camera);
-    app.add_systems<cevy::ecs::core_stage::PreUpdate>(Transform::children_system);
+    app.add_systems<cevy::ecs::core_stage::PostUpdate>(Transform::children_system);
     app.add_systems<ecs::core_stage::PostUpdate>(TransformVelocity::system);
   };
 };
